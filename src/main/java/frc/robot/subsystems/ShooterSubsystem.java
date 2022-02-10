9c6b3e18b4f134c0442c2019cbf2b2a20e835d2c
Micah Rao <raoboys12@gmail.com>
2022-02-09 20:16:57 -0500

    Created PID method for shooter motors

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 
@@ -14,6 +15,7 @@ public class ShooterSubsystem extends SubsystemBase {
     private final CANSparkMax shooterLeft = new CANSparkMax(Ports.SHOOTER_LEFT, MotorType.kBrushless);
     private final TalonSRX intakeRight = new TalonSRX(Ports.INTAKE_RIGHT);
     private final TalonSRX intakeLeft = new TalonSRX(Ports.INTAKE_LEFT);
+    // CANSparkMax.getRelativeEncoder(int)
 
     public ShooterSubsystem() {
         shooterLeft.setInverted(true);
@@ -22,6 +24,21 @@ public class ShooterSubsystem extends SubsystemBase {
     public void setShooterMotors(double speed) {
         shooterRight.set(speed);
         shooterLeft.set(speed);
+    }
+
+    private final double shooter_kP = 0.07;
+    private final double nominalPWM = 0.7;
+    /**
+     * Uses a PID loop to get both shooter motors to a specific, consistent RPM
+     * @param targetSpeed the target speed for the motors (in RPM)
+     */
+    public void setShooterMotorsPID(double targetSpeed) {
+        double error = targetSpeed - getShooterVelocity();
+        double percentOutput = (error * shooter_kP) / 5676; // divide error in rpm by max rpm of neo motor to get percentage
+        percentOutput = Math.min(Math.max(percentOutput, 0), 1);
+
+        shooterRight.set(percentOutput);
+        shooterLeft.set(percentOutput);
     }
 
     public void setIntakeMotors(double speed) {
@@ -34,7 +51,9 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        Shuffleboard.getTab("Shooter Velocity");
+    }
 
     @Override
     public void simulationPeriodic() {}
