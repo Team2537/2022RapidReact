4d49d968d996c0a8e884622b476433cdfb5f41c5
Micah Rao <raoboys12@gmail.com>
2022-02-20 13:47:28 -0500

    Update Ports.java

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -6,8 +6,8 @@ public class Ports {
     public static final int FRONT_RIGHT = 3;
     public static final int BACK_RIGHT = 4;
 
-    public static final int SHOOTER_LEFT = 1;
-    public static final int SHOOTER_RIGHT = 3;
+    public static final int SHOOTER_LEFT = 4;
+    public static final int SHOOTER_RIGHT = 5;
     public static final int INTAKE_LEFT = 0;
     public static final int INTAKE_RIGHT = 8;
 }
